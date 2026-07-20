f73c15d5534e3ab07650415f503840c01a61a0f8
Straightwebdesign-de <straightnorth.sl@gmail.com>
2026-07-20 20:54:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("btnHallo");
+
+button.addEventListener("click", function() {
+
+    document.getElementById("video").style.display = "block";
+
+    document.getElementById("ytVideo").src =
+        "https://www.youtube.com";
+
+});
