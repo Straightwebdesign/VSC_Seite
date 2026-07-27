33a59480caa57d048b65024146b8a8590e8e769d
Straightwebdesign-de <straightnorth.sl@gmail.com>
2026-07-27 21:19:05 +0200

    Remove button click event listener for video display
    löschen der js datei

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const button = document.getElementById("btnHallo");
 
-button.addEventListener("click", function() {
-
-    document.getElementById("video").style.display = "block";
-
-    document.getElementById("ytVideo").src =
-        "https://www.youtube.com";
-
-});
